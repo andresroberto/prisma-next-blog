a3055c296a30499b09a03523ccfe25c7d13631aa
Andrés Rojas <andresroberto10@gmail.com>
2022-08-18 17:31:51 +0200

    Add articles page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/articles'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/articles/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const Articles: NextPage = () => {
+  return (
+    <h1>Hello, Next!</h1>
+  );
+}
+
+export default Articles
